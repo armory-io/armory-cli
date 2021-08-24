a595abfd342effcd589a0dbd66924dd1acc1062f
Justin Field <fieldju@gmail.com>
2021-08-24 09:08:30 -0700

    refactor: gut the cli down to the only the root command

--- cmd/rootCmd.go
+++ cmd/rootCmd.go
@@ -1,11 +1,6 @@
 package cmd
 
 import (
-	"flag"
-	"github.com/armory/armory-cli/cmd/applicationCmd"
-	"github.com/armory/armory-cli/cmd/accountCmd"
-	"github.com/armory/armory-cli/cmd/deployCmd"
-	"github.com/armory/armory-cli/internal/config"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
@@ -19,7 +14,7 @@ var verboseFlag bool
 
 var rootCmd = &cobra.Command{
 	Use:   deployCliName,
-	Short: "Trigger, monitor, and diagnose your deployments",
+	Short: "A CLI for using Armory Cloud",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -29,30 +24,8 @@ func Execute() {
 }
 
 func init() {
-	// Add base commands
-	rootCmd.AddCommand(applicationCmd.BaseCmd)
-	rootCmd.AddCommand(accountCmd.BaseCmd)
-	rootCmd.AddCommand(deployCmd.BaseCmd)
-
 	rootCmd.PersistentFlags().BoolVarP(&verboseFlag, ParamVerbose, "v", false, "show more details")
-	rootCmd.PersistentFlags().StringP(config.ParamContext, "C", "default", "context")
-
-	// Hidden flags
-	rootCmd.PersistentFlags().String(config.ParamEndpoint, "deployCmd.cloud.armory.io:443", "deployCmd engine endpoint")
-
-	rootCmd.PersistentFlags().Bool(config.ParamInsecure, false, "do not verify server certificate")
-	rootCmd.PersistentFlags().Bool(config.ParamPlaintext, false, "use a plaintext connection (warning insecure!)")
-	rootCmd.PersistentFlags().Bool(config.ParamNoProxy, false, "skip system defined proxy (HTTP_PROXY, HTTPS_PROXY)")
-	rootCmd.PersistentFlags().String(config.ParamCacert, "", "path to server certificate authority")
-	rootCmd.PersistentFlags().String(config.ParamCert, "", "path to client certificate (mTLS)")
-	rootCmd.PersistentFlags().String(config.ParamKey, "", "path to client certificate key (mTLS)")
-	rootCmd.PersistentFlags().String(config.ParamKeyPassword, "", "password to the client certificate key (mTLS)")
-	rootCmd.PersistentFlags().String(config.ParamServerName, "", "override server name")
-	rootCmd.PersistentFlags().String(config.ParamToken, "", "authentication token")
-	rootCmd.PersistentFlags().Bool(config.ParamAnonymously, false, "connect anonymously. This will likely fail in a non test environment.")
-
 	rootCmd.PersistentPreRunE = configureLogging
-	rootCmd.SilenceUsage = true
 }
 
 func configureLogging(cmd *cobra.Command, args []string) error {
@@ -62,6 +35,5 @@ func configureLogging(cmd *cobra.Command, args []string) error {
 	}
 	log.SetLevel(lvl)
 	log.SetFormatter(&log.TextFormatter{})
-	_ = flag.Set("logtostderr", "true")
 	return nil
 }
--- go.mod
+++ go.mod
@@ -3,24 +3,7 @@ module github.com/armory/armory-cli
 go 1.16
 
 require (
-	github.com/armory-io/go-cloud-service v0.8.0-79558ea
-	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/golang/protobuf v1.5.2
-	github.com/hashicorp/go-getter v1.5.6
-	github.com/juju/ansiterm v0.0.0-20210706145210-9283cdf370b5
-	github.com/lunixbochs/vtclean v1.0.0 // indirect
-	github.com/mitchellh/mapstructure v1.4.1
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.2.1
-	github.com/spf13/pflag v1.0.5
-	github.com/stretchr/objx v0.2.0 // indirect
-	github.com/stretchr/testify v1.7.0
-	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
-	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
-	golang.org/x/tools v0.1.5 // indirect
-	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c
-	google.golang.org/grpc v1.38.0
-	google.golang.org/protobuf v1.26.0
-	gopkg.in/yaml.v2 v2.4.0
-	k8s.io/apimachinery v0.22.0
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
 )
