4620f9dbd82d508c30f591aee28c5abbd1096210
Ryan Morgan <ryan.morgan@armory.io>
2021-09-27 10:55:04 -0400

    fix: updates to be consistent with the deploy feature

--- cmd/deploy/status.go
+++ cmd/deploy/status.go
@@ -1,8 +1,6 @@
 package deploy
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
 	deploy "github.com/armory-io/deploy-engine/deploy/client"
 	"github.com/armory/armory-cli/cmd"
@@ -22,7 +20,6 @@ const (
 
 type StatusOptions struct {
 	DeploymentId   string
-	Output		   string
 }
 
 func NewDeployStatusCmd(deployOptions *cmd.RootOptions) *cobra.Command {
@@ -34,48 +31,32 @@ func NewDeployStatusCmd(deployOptions *cmd.RootOptions) *cobra.Command {
 		Long:    deployStatusLong,
 		Example: deployStatusExample,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if statusOptions.Output != "" && statusOptions.Output != "json" {
-				return errors.New("the output type is invalid. Do not specify parameter to get plain output. Available options: [json]")
-			}
 			return status(cmd, deployOptions, statusOptions)
 		},
 	}
 	cmd.PersistentFlags().StringVarP(&statusOptions.DeploymentId, "deploymentId", "i", "", "The id of an existing deployment (required)")
-	cmd.PersistentFlags().StringVarP(&statusOptions.Output, "output", "o", "", "Set the output type. Available options: [json]")
 	cmd.MarkPersistentFlagRequired("deploymentId")
 	return cmd
 }
 
 func status(cmd *cobra.Command, deployOptions *cmd.RootOptions, statusOptions *StatusOptions) error {
 	req := deployOptions.DeployClient.DeploymentServiceApi.DeploymentServiceStatus(deployOptions.DeployClient.Context, statusOptions.DeploymentId)
-	deployResp, _, err := req.Execute()
+	deployResp, response, err := req.Execute()
+	if err != nil && response.StatusCode >= 300 {
+		openAPIErr := err.(deploy.GenericOpenAPIError)
+		return fmt.Errorf("deployment returns an error: status code(%d) %s",
+			response.StatusCode, string(openAPIErr.Body()))
+	}
 	var ret string
-	switch output := statusOptions.Output; output {
-	case "json":
-		ret = printJson(deployResp, err)
-	default:
+	if deployOptions.O != "" {
+		ret, err = deployOptions.Output.Formatter(deployResp, err)
+	} else {
 		ret = printPlain(deployOptions, deployResp, statusOptions.DeploymentId, err)
 	}
 
-	fmt.Printf(ret)
+	fmt.Fprintln(cmd.OutOrStdout(), ret)
 
 	return nil
-}
-
-func printJson(deployResp deploy.DeploymentV2DeploymentStatusResponse, err error) string {
-	if err != nil {
-		return getErrorAsJson(err)
-	}
-
-	data, err := json.MarshalIndent(deployResp, "", "  ")
-	if err != nil {
-		return getErrorAsJson(err)
-	}
-	return fmt.Sprintf(string(data))
-}
-
-func getErrorAsJson(err error) string {
-	return fmt.Sprintf("{ \"error\": \"%s\" }", err)
 }
 
 func printPlain(deployOptions *cmd.RootOptions, deployResp deploy.DeploymentV2DeploymentStatusResponse, deploymentId string, err error) string {
