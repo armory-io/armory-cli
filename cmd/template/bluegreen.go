762d9dd9881be9cae7fdba813f6c6dbab5f9038f
Cat Moca <cat.r.mocha@gmail.com>
2022-03-01 17:04:57 -0600

    chore(bluegreen): update blue green template shutDownOldVersionAfter comment

--- cmd/template/bluegreen.go
+++ cmd/template/bluegreen.go
@@ -73,7 +73,7 @@ strategies:
     # After each step completes, Borealis exposes the new version
     # to the activeService.
     #
-    # Finally, Borealis executes the "shutDownOldVersion" steps in parallel.
+    # Finally, Borealis executes the "shutDownOldVersionAfter" steps in parallel.
     # After each step completes, Borealis deletes the old version.
     blueGreen:
 
