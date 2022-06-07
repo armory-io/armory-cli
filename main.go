0454d0ec551ae663cdedca11deb1b2f00629b96c
Cat Moca <cat.r.mocha@gmail.com>
2022-06-06 19:14:25 -0500

    fix(error): errors should be printed once and remove unnecessary deploy engine response err string

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"github.com/armory/armory-cli/cmd"
-	"github.com/spf13/cobra"
 	"os"
 )
 
 func main() {
 	rootCmd := cmd.NewCmdRoot(os.Stdout, os.Stderr)
-	cobra.CheckErr(rootCmd.Execute())
+	rootCmd.Execute()
 }
