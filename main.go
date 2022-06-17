26d3644ab635613ae1fdcb46dbbd3f7dd69daec8
Cat <cat.r.mocha@gmail.com>
2022-06-17 08:24:35 -0500

    fix(error): root cmd errors will have exit code (#66)

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"github.com/armory/armory-cli/cmd"
+	"github.com/spf13/cobra"
 	"os"
 )
 
 func main() {
 	rootCmd := cmd.NewCmdRoot(os.Stdout, os.Stderr)
-	rootCmd.Execute()
+	// required so errors aren't double printed
+	rootCmd.SilenceErrors = true
+	// required so errors have an exit code
+	cobra.CheckErr(rootCmd.Execute())
 }
