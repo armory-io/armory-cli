b868c9f8d4a55f5c09af0009e1701cf0ccf1f24c
Cat Moca <cat.r.mocha@gmail.com>
2022-07-05 11:14:34 -0500

    respond to feedback

--- cmd/template/kubernetes.go
+++ cmd/template/kubernetes.go
@@ -37,8 +37,7 @@ func buildTemplateKubernetesCore(options *templateCanaryOptions) (*yaml.Node, er
 	root.Content = append(root.Content, util.BuildStringNode("kind", "kubernetes", "")...)
 	root.Content = append(root.Content, util.BuildStringNode("application", "<AppName>", "The name of the application to deploy.")...)
 
-	deployConfigNode, deployConfigValuesNode := util.BuildMapNode("deploymentConfig",
-		"")
+	deployConfigNode, deployConfigValuesNode := util.BuildMapNode("deploymentConfig", "")
 	timeoutNode, timeoutValuesNode := util.BuildMapNode("timeout",
 		"Optional. Sets a timeout for rolling back deployments that do not reach a READY state. Applies to all deployments in this file.")
 	timeoutValuesNode.Content = append(timeoutValuesNode.Content, util.BuildIntNode("duration", "1800", "")...)
