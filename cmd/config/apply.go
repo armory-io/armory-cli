ea1af0985a98bbcae88171d634be3c8562ce0abd
Todd Underwood <todd.underwood@armory.io>
2022-08-11 14:48:39 -0700

    feat(roles): refactor to ensure it's easy to add new configuration resources

--- cmd/config/apply.go
+++ cmd/config/apply.go
@@ -68,64 +68,9 @@ func apply(cmd *cobra.Command, options *configApplyOptions, configuration *confi
 	}
 	configClient := configCmd.GetConfigClient(configuration)
 	if payload.Roles != nil {
-		//get existing rolls
-		ctx, cancel := context.WithTimeout(configClient.ArmoryCloudClient.Context, time.Minute)
-		defer cancel()
-		// execute request
-		roles, _, err := configClient.GetRoles(ctx)
+		err := processRoles(configClient, payload.Roles, cmd, payload.AllowAutoDelete)
 		if err != nil {
-			return fmt.Errorf("error getting existing roles: %s", err)
-		}
-		//check to see if roll in config file exists already, if so perform a PUT, if not perform a POST to create
-		exists := false
-		for _, roleInConfig := range payload.Roles {
-			for _, roleInExisting := range roles {
-				if roleInConfig.Name == roleInExisting.Name {
-					exists = true
-					//update existing role
-					ctx, cancel := context.WithTimeout(configClient.ArmoryCloudClient.Context, time.Minute)
-					defer cancel()
-					req, err := configCmd.UpdateRolesRequest(&roleInConfig)
-					_, _, err = configClient.UpdateRole(ctx, req)
-					if err != nil {
-						return fmt.Errorf("error trying to update role: %s", err)
-					}
-					_, err = fmt.Fprintln(cmd.OutOrStdout(), "Updated role: "+roleInConfig.Name)
-					break
-				}
-			}
-			if !exists {
-				//create new role
-				ctx, cancel := context.WithTimeout(configClient.ArmoryCloudClient.Context, time.Minute)
-				defer cancel()
-				req, err := configCmd.CreateRoleRequest(&roleInConfig)
-				_, _, err = configClient.CreateRole(ctx, req)
-				if err != nil {
-					return fmt.Errorf("error trying to update role: %s", err)
-				}
-				_, err = fmt.Fprintln(cmd.OutOrStdout(), "Created role: "+roleInConfig.Name)
-			}
-			exists = false
-		}
-		//Check to see if any existing roles are no longer in the config file, if so delete them
-		deletedRoles := findDeletedRoles(payload.Roles, roles)
-		if !payload.AllowAutoDelete {
-			_, err = fmt.Fprintln(cmd.OutOrStdout(), "Detected the following roles that should be deleted. Doing so may be destructive.")
-			_, err = fmt.Fprintln(cmd.OutOrStdout(), "You can enable deletes by setting 'allowAutoDelete' to 'true' in the configuration file.")
-		}
-		for _, deletedRole := range deletedRoles {
-			if !payload.AllowAutoDelete {
-				_, err = fmt.Fprintln(cmd.OutOrStdout(), deletedRole)
-			} else {
-				ctx, cancel := context.WithTimeout(configClient.ArmoryCloudClient.Context, time.Minute)
-				defer cancel()
-				req, err := configCmd.DeleteRolesRequest(deletedRole)
-				_, err = configClient.DeleteRole(ctx, req)
-				if err != nil {
-					return fmt.Errorf("error trying to delete role: %s", err)
-				}
-				_, err = fmt.Fprintln(cmd.OutOrStdout(), "Deleted role: "+deletedRole)
-			}
+			return err
 		}
 	}
 	return err
@@ -143,4 +88,67 @@ func findDeletedRoles(rolesInConfigFile, existingRoles []model.RoleConfig) []str
 		}
 	}
 	return deletedRoles
+}
+
+func processRoles(configClient *configCmd.ConfigClient, rolesFromConfig []model.RoleConfig, cmd *cobra.Command, allowAutoDelete bool) error {
+	//get existing rolls
+	ctx, cancel := context.WithTimeout(configClient.ArmoryCloudClient.Context, time.Minute)
+	defer cancel()
+	// execute request
+	existingRoles, _, err := configClient.GetRoles(ctx)
+	if err != nil {
+		return fmt.Errorf("error getting existing roles: %s", err)
+	}
+	//check to see if roll in config file exists already, if so perform a PUT, if not perform a POST to create
+	exists := false
+	for _, roleInConfig := range rolesFromConfig {
+		for _, roleInExisting := range existingRoles {
+			if roleInConfig.Name == roleInExisting.Name {
+				exists = true
+				//update existing role
+				ctx, cancel := context.WithTimeout(configClient.ArmoryCloudClient.Context, time.Minute)
+				defer cancel()
+				req, err := configCmd.UpdateRolesRequest(&roleInConfig)
+				_, _, err = configClient.UpdateRole(ctx, req)
+				if err != nil {
+					return fmt.Errorf("error trying to update role: %s", err)
+				}
+				_, err = fmt.Fprintln(cmd.OutOrStdout(), "Updated role: "+roleInConfig.Name)
+				break
+			}
+		}
+		if !exists {
+			//create new role
+			ctx, cancel := context.WithTimeout(configClient.ArmoryCloudClient.Context, time.Minute)
+			defer cancel()
+			req, err := configCmd.CreateRoleRequest(&roleInConfig)
+			_, _, err = configClient.CreateRole(ctx, req)
+			if err != nil {
+				return fmt.Errorf("error trying to update role: %s", err)
+			}
+			_, err = fmt.Fprintln(cmd.OutOrStdout(), "Created role: "+roleInConfig.Name)
+		}
+		exists = false
+	}
+	//Check to see if any existing roles are no longer in the config file, if so delete them
+	deletedRoles := findDeletedRoles(rolesFromConfig, existingRoles)
+	if !allowAutoDelete {
+		_, err = fmt.Fprintln(cmd.OutOrStdout(), "Detected the following roles that should be deleted. Doing so may be destructive.")
+		_, err = fmt.Fprintln(cmd.OutOrStdout(), "You can enable deletes by setting 'allowAutoDelete' to 'true' in the configuration file.")
+	}
+	for _, deletedRole := range deletedRoles {
+		if !allowAutoDelete {
+			_, err = fmt.Fprintln(cmd.OutOrStdout(), deletedRole)
+		} else {
+			ctx, cancel := context.WithTimeout(configClient.ArmoryCloudClient.Context, time.Minute)
+			defer cancel()
+			req, err := configCmd.DeleteRolesRequest(deletedRole)
+			_, err = configClient.DeleteRole(ctx, req)
+			if err != nil {
+				return fmt.Errorf("error trying to delete role: %s", err)
+			}
+			_, err = fmt.Fprintln(cmd.OutOrStdout(), "Deleted role: "+deletedRole)
+		}
+	}
+	return nil
 }
