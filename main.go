4cbf60dd9ed1323704ba9056c04cccdfaaf4e02a
Daniel Peach <daniel.peach@armory.io>
2023-02-21 17:50:53 -0500

    fix: cli should print to stdout (#110)

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/armory/armory-cli/cmd"
 	"github.com/spf13/cobra"
 	"os"
@@ -10,7 +11,12 @@ func main() {
 	// Disabling EnableCommandSorting allows us to set our own command sort order.
 	cobra.EnableCommandSorting = false
 
-	rootCmd := cmd.NewCmdRoot(os.Stdout, os.Stderr)
+	rootCmd, err := cmd.NewCmdRoot(os.Stdout, os.Stderr)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		os.Exit(1)
+	}
+
 	// required so errors aren't double printed
 	rootCmd.SilenceErrors = true
 	// required so errors have an exit code
