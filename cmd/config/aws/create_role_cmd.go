47cf7c60cadd1f04b3433cee130a14a5d68b3170
Jacob Kobernik <jacob.kobernik@armory.io>
2023-11-10 11:45:54 -0500

    feat(aws): create-role cmd uses new template w/ role name param and arn output (#205)
    * feat(aws): create-role command uses new template location wth role name param and arn output
    
    * fix(aws): edit to pass static check
    
    * chore(aws): incorporate feedback
    
    * Apply suggestions from code review
    
    Co-authored-by: Daniel Peach <daniel.peach@armory.io>
    
    * fix(aws): typo in docstring
    
    * Apply suggestions from code review
    
    Co-authored-by: aimee <aimeeu.opensource@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: aimee <aimeeu.opensource@gmail.com>
    
    * Update cmd/config/aws/create_role_cmd.go
    
    Co-authored-by: Daniel Peach <daniel.peach@armory.io>
    
    * Update cmd/config/aws/create_role_cmd.go
    
    Co-authored-by: aimee <aimeeu.opensource@gmail.com>
    
    ---------
    
    Co-authored-by: Daniel Peach <daniel.peach@armory.io>
    Co-authored-by: aimee <aimeeu.opensource@gmail.com>

--- cmd/config/aws/create_role_cmd.go
+++ cmd/config/aws/create_role_cmd.go
@@ -11,14 +11,28 @@ import (
 	"github.com/spf13/cobra"
 	log "go.uber.org/zap"
 	"io"
+	"time"
+)
+
+var (
+	useDevTemplate bool
 )
 
 const (
-	configCreateRoleShort = "AWS Create Role via CloudFormation Stack"
-	//TODO For usage documentation, visit...
-	configCreateRoleLong    = "Use AWS CloudFormation quick create to run a template that allows Armory to assume a role to manage deployments\n"
+	configCreateRoleShort   = "AWS Create Role via CloudFormation Stack"
+	configCreateRoleLong    = "Use Armory's AWS CloudFormation template to create a Stack with an IAM role. This IAM Role allows Armory to manage your Lambda deployments.\n"
 	configCreateRoleExample = "armory config aws create-role"
-	templateUrl             = "https://us-east-1.console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks/quickcreate?templateURL=https://armory-cdaas-cloudformation.s3.us-west-2.amazonaws.com/iam-role-cfn.template&stackName=Armory-CDAAS-Role-Stack&param_AccountId=%s&param_ExternalId=%s"
+	templateUrl             = "https://us-east-1.console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks/quickcreate?templateURL=https://s3.us-west-2.amazonaws.com/static.cloud.armory.io/templates/aws/cloudformation/%siam-role-cfn.template&stackName=Armory-CDAAS-Role-Stack-%s&param_AccountId=%s&param_ExternalId=%s&param_ArmoryRoleName=%s"
+	devTemplatePrefix       = "dev-"
+	armoryRoleName          = "ArmoryRole"
+
+	installPromptIntro  = "In order to deploy AWS resources, Armory needs to create a Trust Relationship in your AWS account by adding an IAM role that Armory can assume to execute deployments on your behalf."
+	installPromptPrereq = "Prerequisite: In your default browser, log in to the AWS Account you want to connect to Armory CD-as-a-Service. You must have permission to configure IAM roles."
+	installPromptStep1  = "1. Type \"Y\" to begin Stack creation. This opens your browser to the CloudFormation page of your AWS Console. You complete the rest of this process in your browser."
+	installPromptStep2  = "2. Review the resources that Armory is creating in your AWS account. The default IAM Role name is \"ArmoryRole\" but you can customize the name of that role if you wish."
+	installPromptStep3  = "3. Click \"Create\" on the AWS CloudFormation page and wait for Stack creation to complete."
+	installPromptStep4  = "4. After the CloudFormation Stack creation finishes, locate the created role ARN in the \"Outputs\" section. You can find it under the key \"RoleArnOutput\". You use the ARN to populate the \"deployAsIamRole\" field in your CD-as-a-Service Lambda deployment target."
+	installPromptErr    = "Unable to open browser. Copy and paste the following URL into your browser:"
 )
 
 func NewCreateRoleCmd(configuration *cliconfig.Configuration, reader io.ReadCloser) *cobra.Command {
@@ -35,6 +49,14 @@ func NewCreateRoleCmd(configuration *cliconfig.Configuration, reader io.ReadClos
 			return createRole(cmd, configuration, reader)
 		},
 	}
+
+	// Note: when wanting to develop the cloudformation template being used by this command you can pass the `--dev-template` flag and then the command will use
+	// `dev-iam-role-cfn.template` instead of `iam-role-cfn.template`. This allows you to make changes to the dev template and not worry about effecting users.
+	cmd.Flags().BoolVar(&useDevTemplate, "dev-template", false, "")
+	if err := cmd.Flags().MarkHidden("dev-template"); err != nil {
+		return nil
+	}
+
 	return cmd
 }
 
@@ -50,14 +72,23 @@ func createRole(cmd *cobra.Command, cli *cliconfig.Configuration, reader io.Read
 	}
 
 	externalID := fmt.Sprintf("%s:%s", orgID, envID)
+	epochTs := fmt.Sprintf("%d", time.Now().Unix())
+	templatePrefix := ""
+
+	if useDevTemplate {
+		fmt.Println("** Using Dev Template: https://static.cloud.armory.io/templates/aws/cloudformation/dev-iam-role-cfn.template")
+		templatePrefix = devTemplatePrefix
+	}
 
 	cmd.SilenceUsage = true
 	browser.Stderr = io.Discard
 	browser.Stdout = io.Discard
-	log.S().Info("In order to deploy AWS resources, we need to create a Trust Relationship in your AWS account by adding a role that Armory can assume to execute deployments on your behalf.")
-	log.S().Info("1. Log in to the AWS Management Console you want to connect to in your default browser. The logged in user requires access to configure IAM roles.")
-	log.S().Info("2. Click \"Create\" in the AWS CloudFormation screen and wait for the stack creation to complete.")
-	log.S().Info("3. Once the CloudFormation is finished, locate the ArmoryRole by navigating to IAM > Roles in the AWS Management console. Use the `arn` value as the `account` in your CD-as-a-Service deployment targets.")
+	log.S().Info(installPromptIntro)
+	log.S().Info(installPromptPrereq)
+	log.S().Info(installPromptStep1)
+	log.S().Info(installPromptStep2)
+	log.S().Info(installPromptStep3)
+	log.S().Info(installPromptStep4)
 	prompt := promptui.Prompt{
 		Label:     "Continue",
 		IsConfirm: true,
@@ -73,10 +104,10 @@ func createRole(cmd *cobra.Command, cli *cliconfig.Configuration, reader io.Read
 		}
 		return err
 	}
-	url := fmt.Sprintf(templateUrl, cli.GetArmoryCloudEnvironmentConfiguration().AWSAccountID, externalID)
+	url := fmt.Sprintf(templateUrl, templatePrefix, epochTs, cli.GetArmoryCloudEnvironmentConfiguration().AWSAccountID, externalID, armoryRoleName)
 	err = browser.OpenURL(url)
 	if err != nil {
-		log.S().Info("Unable to open browser. Please copy and paste the following URL into your browser.")
+		log.S().Info(installPromptErr)
 		log.S().Info(url)
 	}
 
